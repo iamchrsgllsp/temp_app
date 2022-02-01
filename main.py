b38455c2173dc473bcb814bbc6f079dfc0c0a89b
iamchrsgllsp <chrisgillespie2001@googlemail.com>
2022-02-01 23:09:58 +0000

    fix routes to work

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, url_for
+import database
 
 app = Flask(__name__)
 
 
-import routes
+@app.route('/', methods=['GET'])
+def test():
+    data = database.enter()
+    return render_template("home.html",data = data)
 
