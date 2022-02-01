20484915a86b36fd2360fa967e1ab188444f2f8d
iamchrsgllsp <chrisgillespie2001@goooglemail.com>
2022-02-01 22:32:35 +0000

    added boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from distutils.log import debug
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+# import declared routes
+import routes
+
+app.run(debug=True)
