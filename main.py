adb5503f57e17ae79afcd74c4c0d1f296ab5c5f7
iamchrsgllsp <chrisgillespie2001@goooglemail.com>
2022-02-01 23:49:39 +0000

    added more style and login page

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from flask import Flask, render_template, request, url_for
 import database
 
@@ -9,4 +10,9 @@ def test():
     data = database.enter()
     return render_template("home.html",data = data)
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
+
+app.run(debug=True)
