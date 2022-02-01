023947ea4dbec3e035e22bc6d3d5cce18b3752e5
iamchrsgllsp <chrisgillespie2001@goooglemail.com>
2022-02-01 23:01:00 +0000

    add routes

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def test():
-    return render_template("home.html")
+import routes
 
